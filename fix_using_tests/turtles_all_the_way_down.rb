7b6cb068c63ccbb1f41f08cf67518d710fd0975c
NickyEXE <dovern42@gmail.com>
2019-02-25 20:05:04 -0500

    done

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def turtles
     [
       {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
@@ -10,8 +10,10 @@ end
 
 def turtle_traits(turtles)
   turtles.map do |turtle|
-    turtle[:traits].each do |trait|
-      trait
+    if turtle.kind_of?(Array)
+      turtle[:traits].flatten
+    else
+      [turtle[:traits]].flatten
     end
   end
 end
